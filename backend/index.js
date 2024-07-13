00b9b4b9bbe091d69214bf7c45385befd2d6a1fc
Yuvi <sryuvi51214@gmail.com>
2024-07-13 18:14:49 +0530

    initialized zod

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,19 @@
 const express=require("express")
+const {createTodo,updateTodo} = require("./types")
+const app=express()
+app.use(express.json())
+
+app.post('/todo',(req,res)=>{
+
+})
+
+app.get('/todos',(req,res)=>{
+
+})
+
+app.put('/completed',(req,res)=>{
+
+})
+
+
+app.listen(3000)
