f7e0c6570c06634dd385034aba7feb33e7c1381b
Yuvi <sryuvi51214@gmail.com>
2024-07-13 18:43:35 +0530

    ek aur commit

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,16 @@ const app=express()
 app.use(express.json())
 
 app.post('/todo',(req,res)=>{
-
+    const response=createTodo.safeParse(req.body)
+    if(!response.success){
+        res.status(411).json({
+            mssg:"you sent the wrong inputs"
+        })
+        return;
+    }
+    res.status(200).json({
+        mssg:"user added successfully"
+    })
 })
 
 app.get('/todos',(req,res)=>{
@@ -12,7 +21,16 @@ app.get('/todos',(req,res)=>{
 })
 
 app.put('/completed',(req,res)=>{
-
+    const response=updateTodo.safeParse(req.body)
+    if(!response.success){
+        res.status(411).json({
+            mssg :"wrong inputs"
+        })
+        return;
+    }
+    res.status(200).json({
+        mssg:"updated successfully"
+    })
 })
 
 
